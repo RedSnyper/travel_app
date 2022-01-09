36134ddf9d2876fd053fac77e7101de5e50b8ce5
Red Snyper <zbon77@gmail.com>
2022-01-09 17:27:58 +0545

    fixing imports

--- travel_app/main.py
+++ travel_app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from .database import *
+from . import models
+from .database.db import engine
+
+
+models.Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI()
+
 
 
 @app.get("/")
---
+++ travel_app/models/comment.py
@@ -0,0 +1,3 @@
+from ..database.db import Base
+
+print('HI')
